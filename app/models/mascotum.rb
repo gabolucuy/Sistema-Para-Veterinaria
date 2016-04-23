e5c16beb7136aba6220e7d190fb7e13491701ac6
Esteban Zurita <estebanzurita316@gmail.com>
2016-04-23 10:58:20 -0400

    p

--- app/models/mascotum.rb
+++ app/models/mascotum.rb
@@ -1,7 +1,9 @@
 class Mascotum < ActiveRecord::Base
 	belongs_to :cliente
 	validates :nombre, :especie, :raza, :fecha_nacimiento, :clientes_id, presence: true
-	before_create :fecha_nac_verdad?
+	before_create :fecha_nac_verdad? 
+
+	
 	def fecha_nac_verdad?
 		if( fecha_nacimiento < Date.today)
 			return true
