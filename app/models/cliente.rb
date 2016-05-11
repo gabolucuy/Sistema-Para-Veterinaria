c2c8770a927f9bcab063ab62c369dd133e555db0
gabolucuy <gabolucuy@hotmail.com>
2016-05-11 16:19:11 -0400

    Arreglo de errores en espanol

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,14 +1,13 @@
 class Cliente < ActiveRecord::Base
-	validates :correo,:presence=>{message: "no puede estar vacio"}
+	validates :ci, :uniqueness=>{message:"ya esta registrado"}
+	validates :ci,:presence=>{message: "no puede estar vacio"}
 	validates :nombre,:presence=>{message: "no puede estar vacio"}
 	validates :apellido,:presence=>{message: "no puede estar vacio"}
 	validates :telefono,:presence=>{message: "no puede estar vacio"}
-	validates :ci,:presence=>{message: "no puede estar vacio"}
-
+	validates :correo,:presence=>{message: "no puede estar vacio"}
+	validates :correo, uniqueness: true
 	has_many :mascotums
 	has_many :ventums
-	validates :ci,:correo, uniqueness: true
-	validates :correo, :ci, uniqueness: true
 	before_create :correo?
 def correo?
 	a=self.correo.split('@')
